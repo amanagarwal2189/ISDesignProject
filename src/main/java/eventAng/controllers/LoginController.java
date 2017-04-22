2827462ec0e06b50323425ec03f93a86766c4ea0
Aman Agarwal <aman.agarwal2189@gmail.com>
2017-04-21 21:54:14 -0500

    dao addition

--- src/main/java/eventAng/controllers/LoginController.java
+++ src/main/java/eventAng/controllers/LoginController.java
@@ -1,8 +1,8 @@
 package eventAng.controllers;
 
 import java.io.IOException;
-import java.sql.ResultSet;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
@@ -13,116 +13,136 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.PreparedStatement;
-
-import eventAng.database.connection.DBConnection;
-import eventAng.domain.User;
-
+import eventAng.domain.Host;
+import eventAng.domain.Sponsor;
+import eventAng.model.HostDao;
+import eventAng.model.SponsorDao;
 
 @Controller
 public class LoginController {
 
-	@Autowired 
-	 private HttpSession httpSession;
-	
-	
+	@Autowired
+	private HttpSession httpSession;
+
+	@Autowired
+	HostDao hostDao;
+
+	@Autowired
+	SponsorDao sponsorDao;
+
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	@ResponseBody
-	public void login(@RequestParam String emailId,@RequestParam String password, HttpServletResponse response) throws IOException {
-	
-		//int countUser=0;
-		User users = new User();
-		//System.out.println(emailId);
-		users = fetchOrganizerDetails(emailId);
-		boolean lFlag =validateUsers(password, users.getPassword());
-        System.out.print("Password is "+password);
-       
-		if(lFlag){
-		httpSession.setAttribute("users", users);
-		response.setContentType("text/html;charset=UTF-8");
-        response.getWriter().write("True");
-		
-		}
-		else{
-			//request.setAttribute("error","Invalid user");
-			//System.out.println("Invalid");
-			response.setContentType("text/html;charset=UTF-8");
-	        response.getWriter().write("False");
-			
-		}
-	}
-	
-	
-//	@RequestMapping(value = "/login", method = RequestMethod.POST)
-//    public String login(HttpServletRequest request, Model model) {
-//        String email_id=request.getParameter("email_id");
-//        String password=request.getParameter("password");
-//        //LoginVerify loginVerify = new LoginVerify();
-//        //String message=loginVerify.loginVerification(email_id, password);
-//        //System.out.println("Name is"+email_id+" "+ "password:"+password);
-//		Users users = new Users();
-//		users = fetchOrganizerDetails(email_id);
-//		boolean lFlag =validateUsers(password, users.getPassword());
-//        
-//		if(lFlag){
-//		httpSession.setAttribute("users", users);
-//		
-//		
-//        return "dashboard";
-//		}
-//		else{
-//			request.setAttribute("error","Invalid user");
-//			System.out.println("Invalid");
-//			return "login";
-//			
-//		}
-//    }
-	
-	public User fetchOrganizerDetails(String pEmail){
-		
-		PreparedStatement lPstmnt = null;
-		ResultSet lRst			  = null;
-		User users 				  = new User();
-		
-		try{
-			StringBuilder lBuilder = new StringBuilder("select * from tb_host_dtls where email_id=? and is_active='Y'");
-									 
-			Connection con=DBConnection.getConnection();   
-			lPstmnt = (PreparedStatement) con.prepareStatement(lBuilder.toString());
-			lPstmnt.setString(1, pEmail);
-			lRst=lPstmnt.executeQuery();
-			System.out.println(pEmail);
-			while(lRst.next()){
-				users.setId(lRst.getInt(1));
-				users.setName(lRst.getString(2));
-				users.setEmail_id(lRst.getString(3));
-				users.setPhone(lRst.getString(4));
-				users.setAddress_line_1(lRst.getString(5));
-				users.setAddress_line_2(lRst.getString(6));
-				users.setCity(lRst.getString(7));
-				users.setState(lRst.getString(8));
-				users.setZip(lRst.getInt(9));
-				users.setIs_active(lRst.getString(10));
-				users.setRegd_on(lRst.getDate(11));
-				users.setPassword(lRst.getString(12));
+	public void login(@RequestParam String userType, @RequestParam String emailId, @RequestParam String password, HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+
+		// int countUser=0;
+		// User users = new User();
+		String emptyString = "";
+		boolean lFlag = false;
+		String user_name = emptyString;
+		String user_id = emptyString;
+		//userType = request.getParameter("userType");
+		if (userType.equals("organizer")) {
+			Host host = hostDao.findByEmailId(emailId);
+			if (host.isIs_active()) {
+				lFlag = validateUser(password, host.getPassword());
+				user_id = String.valueOf(host.getId());
+				user_name = host.getName();
 			}
-			
-		}catch(Exception e){
-			System.out.println(e);
+		} else {
+			Sponsor sponsor = sponsorDao.findByEmailId(emailId);
+			if (sponsor.getIs_active()) {
+				lFlag = validateUser(password, sponsor.getPassword());
+				user_id = String.valueOf(sponsor.getId());
+				user_name = sponsor.getName();
+			}
 		}
-		return users;
+		// System.out.println(emailId);
+		// users = fetchOrganizerDetails(emailId);
+		// boolean lFlag =validateUsers(password, users.getPassword());
+		System.out.print("Password is " + password);
+
+		if (lFlag) {
+			// httpSession.setAttribute("userid", users);
+			httpSession.setAttribute("user_emailId", emailId);
+			httpSession.setAttribute("user_id", user_id);
+			httpSession.setAttribute("user_name", user_name);
+			response.setContentType("text/html;charset=UTF-8");
+			response.getWriter().write("True");
+		} else {
+			// request.setAttribute("error","Invalid user");
+			// System.out.println("Invalid");
+			response.setContentType("text/html;charset=UTF-8");
+			response.getWriter().write("False");
+		}
 	}
-	public Boolean validateUsers(String lTypedPassword, String lStoredPassword ){
-		
-		boolean lFlag=false;
-		
-		if(lTypedPassword.equals(lStoredPassword)){
-			lFlag=true;
-		}else{
-			lFlag=false;
+
+	public Boolean validateUser(String lTypedPassword, String lStoredPassword) {
+
+		boolean lFlag = false;
+
+		if (lTypedPassword.equals(lStoredPassword)) {
+			lFlag = true;
+		} else {
+			lFlag = false;
 		}
 		return lFlag;
 	}
 
+	// @RequestMapping(value = "/login", method = RequestMethod.POST)
+	// public String login(HttpServletRequest request, Model model) {
+	// String email_id=request.getParameter("email_id");
+	// String password=request.getParameter("password");
+	// //LoginVerify loginVerify = new LoginVerify();
+	// //String message=loginVerify.loginVerification(email_id, password);
+	// //System.out.println("Name is"+email_id+" "+ "password:"+password);
+	// Users users = new Users();
+	// users = fetchOrganizerDetails(email_id);
+	// boolean lFlag =validateUsers(password, users.getPassword());
+	//
+	// if(lFlag){
+	// httpSession.setAttribute("users", users);
+	//
+	//
+	// return "dashboard";
+	// }
+	// else{
+	// request.setAttribute("error","Invalid user");
+	// System.out.println("Invalid");
+	// return "login";
+	//
+	// }
+	// }
+
+	/*
+	 * public User fetchOrganizerDetails(String pEmail){
+	 * 
+	 * PreparedStatement lPstmnt = null; ResultSet lRst = null; User users = new
+	 * User();
+	 * 
+	 * try{ StringBuilder lBuilder = new
+	 * StringBuilder("select * from tb_host_dtls where email_id=? and is_active='Y'"
+	 * );
+	 * 
+	 * Connection con=DBConnection.getConnection(); lPstmnt =
+	 * (PreparedStatement) con.prepareStatement(lBuilder.toString());
+	 * lPstmnt.setString(1, pEmail); lRst=lPstmnt.executeQuery();
+	 * System.out.println(pEmail); while(lRst.next()){
+	 * users.setId(lRst.getInt(1)); users.setName(lRst.getString(2));
+	 * users.setEmail_id(lRst.getString(3)); users.setPhone(lRst.getString(4));
+	 * users.setAddress_line_1(lRst.getString(5));
+	 * users.setAddress_line_2(lRst.getString(6));
+	 * users.setCity(lRst.getString(7)); users.setState(lRst.getString(8));
+	 * users.setZip(lRst.getInt(9)); users.setIs_active(lRst.getString(10));
+	 * users.setRegd_on(lRst.getDate(11));
+	 * users.setPassword(lRst.getString(12)); }
+	 * 
+	 * }catch(Exception e){ System.out.println(e); } return users; } public
+	 * Boolean validateUsers(String lTypedPassword, String lStoredPassword ){
+	 * 
+	 * boolean lFlag=false;
+	 * 
+	 * if(lTypedPassword.equals(lStoredPassword)){ lFlag=true; }else{
+	 * lFlag=false; } return lFlag; }
+	 */
 }
